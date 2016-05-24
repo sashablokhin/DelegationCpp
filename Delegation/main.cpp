171823b000ac7d9057ac1a4fe76de6445b347788
sashablokhin <sashablokhindev@gmail.com>
2016-05-24 12:26:02 +0300

    Create project

---
+++ Delegation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Delegation
+//
+//  Created by Alexander Blokhin on 24.05.16.
+//  Copyright © 2016 Alexander Blokhin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
